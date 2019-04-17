4ef6dc308497fc616fa1437789aa8bbf40f3e516
배규식 <ksbae1214@naver.com>
2019-04-17 19:03:54 +0900

    Initial Commit

---
+++ KkyuTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KkyuTalk
+//
+//  Created by 배규식 on 17/04/2019.
+//  Copyright © 2019 ksbae1214. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
